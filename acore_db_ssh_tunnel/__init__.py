5c221400fcec4a92fd4f55d9a894e90acecdafc2
Sanhe Hu <MacHu-GWU@users.noreply.github.com>
2023-06-16 00:38:40 -0400

    [no ci] 0.1.1

--- acore_db_ssh_tunnel/__init__.py
+++ acore_db_ssh_tunnel/__init__.py
@@ -1,12 +1,12 @@
 # -*- coding: utf-8 -*-
 
 """
-Package Description.
+Create Database SSH Tunnel for Azerothcore World of Warcraft MySQL Database.
 """
 
 from ._version import __version__
 
-__short_description__ = "Package short description."
+__short_description__ = "Create Database SSH Tunnel for Azerothcore World of Warcraft MySQL Database"
 __license__ = "MIT"
 __author__ = "Sanhe Hu"
 __author_email__ = "husanhe@gmail.com"
