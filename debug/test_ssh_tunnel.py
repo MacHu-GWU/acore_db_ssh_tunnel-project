4d6c9a203f4f796c478825528fab779ed6d9ab03
Sanhe Hu <MacHu-GWU@users.noreply.github.com>
2023-06-16 00:30:20 -0400

    Update test_ssh_tunnel.py

--- debug/test_ssh_tunnel.py
+++ debug/test_ssh_tunnel.py
@@ -1,10 +1,10 @@
 # -*- coding: utf-8 -*-
 
-from acore_db_ssh_tunnel import ssh_tunnel
+from acore_db_ssh_tunnel import api
 
 
 def create_ssh_tunnel():
-    ssh_tunnel.create_ssh_tunnel(
+    api.create_ssh_tunnel(
         path_pem_file=path_pem_file,
         db_host=db_host,
         db_port=db_port,
@@ -14,11 +14,11 @@ def create_ssh_tunnel():
 
 
 def list_ssh_tunnel():
-    ssh_tunnel.list_ssh_tunnel(path_pem_file)
+    api.list_ssh_tunnel(path_pem_file)
 
 
 def test_ssh_tunnel():
-    ssh_tunnel.test_ssh_tunnel(
+    api.test_ssh_tunnel(
         db_port=db_port,
         db_username=db_username,
         db_password=db_password,
@@ -27,20 +27,20 @@ def test_ssh_tunnel():
 
 
 def kill_ssh_tunnel():
-    ssh_tunnel.kill_ssh_tunnel(path_pem_file)
+    api.kill_ssh_tunnel(path_pem_file)
 
 
-db_host = "my-server.1a2b3c4d5e6f.us-east-1.rds.amazonaws.com"
-db_port = 3306
-db_username = "admin"
-db_password = "admin"
-db_name = "my_database"
-jump_host_username = "ubuntu"
-jump_host_public_ip = "111.111.111.111"
-path_pem_file = "/Users/myusername/ec2-key.pem"
+if __name__ == "__main__":
+    db_host = "my-server.1a2b3c4d5e6f.us-east-1.rds.amazonaws.com"
+    db_port = 3306
+    db_username = "admin"
+    db_password = "admin"
+    db_name = "my_database"
+    jump_host_username = "ubuntu"
+    jump_host_public_ip = "111.111.111.111"
+    path_pem_file = "/Users/myusername/ec2-key.pem"
 
-
-create_ssh_tunnel()
-list_ssh_tunnel()
-test_ssh_tunnel()
-kill_ssh_tunnel()
+    # create_ssh_tunnel()
+    # list_ssh_tunnel()
+    # test_ssh_tunnel()
+    # kill_ssh_tunnel()
